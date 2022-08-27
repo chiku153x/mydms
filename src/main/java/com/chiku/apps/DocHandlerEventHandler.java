6c47e2414b6ae9b0c39d1ccf4cc7c76cbb5661d4
bstchinthaka <88303934+bstchinthaka@users.noreply.github.com>
2022-08-27 22:54:19 +0530

    file upload working

--- src/main/java/com/chiku/apps/DocHandlerEventHandler.java
+++ src/main/java/com/chiku/apps/DocHandlerEventHandler.java
@@ -20,8 +20,15 @@ import de.abas.erp.axi2.annotation.EventHandler;
 import de.abas.erp.axi2.event.ButtonEvent;
 import de.abas.erp.axi2.type.ButtonEventType;
 import de.abas.erp.db.DbContext;
+import de.abas.erp.db.Query;
 import de.abas.erp.db.infosystem.custom.ow1.DocHandler;
+import de.abas.erp.db.schema.company.CompanyData;
+import de.abas.erp.db.selection.Conditions;
+import de.abas.erp.db.selection.SelectionBuilder;
 import de.abas.erp.jfop.rt.api.annotation.RunFopWith;
+import de.abas.erp.axi2.type.ScreenEventType;
+import de.abas.erp.axi2.event.ScreenEvent;
+import de.abas.erp.axi2.annotation.ScreenEventHandler;
 
 @RunFopWith(EventHandlerRunner.class)
 @EventHandler(head = DocHandler.class, row = DocHandler.Row.class)
@@ -30,30 +37,34 @@ public class DocHandlerEventHandler {
 	@ButtonEventHandler(field = "start", type = ButtonEventType.AFTER)
 	public void startButtonAfter(ButtonEvent event, ScreenControl screenControl, DbContext ctx, DocHandler head)
 			throws EventException {
-		ctx.out().print("XXX");
-		// MultipartFileUploader.uploadFile(ctx, head.getYmydmsfilepath(),
-		// "http://192.168.1.71:8080");
-
 		CloseableHttpClient httpClient = HttpClients.createDefault();
-		HttpPost uploadFile = new HttpPost("http://192.168.1.71:8080");
+		// HttpPost uploadFile = new HttpPost("http://192.168.1.71:8080");
+		HttpPost uploadFile = new HttpPost(head.getYmydmsurl());
 		MultipartEntityBuilder builder = MultipartEntityBuilder.create();
-		builder.addTextBody("field1", "yes", ContentType.TEXT_PLAIN);
+		builder.addTextBody("payload", head.getYmydmsjson(), ContentType.TEXT_PLAIN);
 
-		// This attaches the file to the POST:
 		File f = new File(head.getYmydmsfilepath());
 		try {
-			ctx.out().print("yyy");
 			builder.addBinaryBody("file", new FileInputStream(f), ContentType.APPLICATION_OCTET_STREAM, f.getName());
 			HttpEntity multipart = builder.build();
 			uploadFile.setEntity(multipart);
 			CloseableHttpResponse response = httpClient.execute(uploadFile);
 			HttpEntity responseEntity = response.getEntity();
-			ctx.out().print("zzz" + " " + responseEntity.getContent());
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			ctx.out().println(e.getMessage());
 		}
 
+	}
+
+	@ScreenEventHandler(type = ScreenEventType.ENTER)
+	public void screenEnter(ScreenEvent event, ScreenControl screenControl, DbContext ctx, DocHandler head)
+			throws EventException {
+		SelectionBuilder<CompanyData> selectionBuilder = SelectionBuilder.create(CompanyData.class);
+		selectionBuilder.add(Conditions.eq(CompanyData.META.idno, "1"));
+		Query<CompanyData> queryCompanyData = ctx.createQuery(selectionBuilder.build());
+		for (CompanyData companyData : queryCompanyData) {
+			head.setYmydmsurl(companyData.getYmydmsresturl());
+		}
 	}
 
 }
